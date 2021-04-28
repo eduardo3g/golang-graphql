dce9fd3002efcb46e0acff74f7e6a8a0ab8fbcd2
eduardo3g <eduardosbrasil10@gmail.com>
2021-04-28 19:32:41 -0300

    feat: implement Categories, Courses and Chapters graphql queries

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,9 @@
 package graph
 
-// This file will not be regenerated automatically.
-//
-// It serves as dependency injection for your app, add any dependencies you require here.
+import "github.com/codeedu/fc2-graphql/graph/model"
 
-type Resolver struct{}
+type Resolver struct {
+	Categories []*model.Category
+	Courses    []*model.Course
+	Chapters   []*model.Chapter
+}
